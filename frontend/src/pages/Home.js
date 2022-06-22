79559ac4214f7303701aeedaa17387419155b50b
Fat2G <fatougassa@gmail.com>
2022-06-22 21:58:40 +0200

    création page d'accueil

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormLogin from '../components/FormLogin';
+import HeaderLogo from '../components/HeaderLogo';
+
+const index = () => {
+  return (
+    <div>
+      <HeaderLogo/>
+      <FormLogin/>    
+    </div>
+  );
+};
+
+export default index;
