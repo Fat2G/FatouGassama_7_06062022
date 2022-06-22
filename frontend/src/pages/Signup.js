34c5ec686e0fb58e60f8b7d3872e7ebb1cc4b774
Fat2G <fatougassa@gmail.com>
2022-06-23 01:54:55 +0200

    création page signup

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormSignup from '../components/FormSignup';
+import HeaderLogo from '../components/HeaderLogo';
+
+
+const Signup = () => {
+  return (
+    <div>
+      <HeaderLogo/>
+      <FormSignup/>
+    </div>
+  );
+};
+
+export default Signup;
