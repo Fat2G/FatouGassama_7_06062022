27bbce1d74f7105f6e8f7fbe8221a25bae1176f1
Fat2G <fatougassa@gmail.com>
2022-07-03 23:00:12 +0200

    création page inscription

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import FormSignup from '../components/FormSignup';
-import HeaderLogo from '../components/HeaderLogo';
-
+import { useEffect, React } from "react";
+import FormSignup from "../components/FormSignup";
+import HeaderLogo from "../components/HeaderLogo";
 
 const Signup = () => {
+  useEffect(() => {
+    // Utilisation du hook d'effet en enlevant la class non voulue aubody afin d'ajouter une.
+    document.body.classList.remove("login");
+    document.body.classList.add("signup");
+  }, []);
+  
   return (
     <div>
-      <HeaderLogo/>
-      <FormSignup/>
+      <HeaderLogo />
+      <FormSignup />
     </div>
   );
 };
