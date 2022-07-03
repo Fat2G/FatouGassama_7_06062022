f40446c1589efc4977f0022260379ac3ab593da6
Fat2G <fatougassa@gmail.com>
2022-07-03 22:59:44 +0200

    création page index

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import FormLogin from '../components/FormLogin';
-import HeaderLogo from '../components/HeaderLogo';
+import { useEffect, React } from "react";
+import FormLogin from "../components/FormLogin";
+import HeaderLogo from "../components/HeaderLogo";
 
-const index = () => {
+const Index = () => {
+  // Utilisation du hook d'effet en enlevant la class non voulue au body afin d'ajouter une.
+  useEffect(() => {
+    document.body.classList.remove("signup");
+    document.body.classList.add("login");    
+  }, []);
+
   return (
     <div>
-      <HeaderLogo/>
-      <FormLogin/>    
+      <HeaderLogo />
+      <FormLogin />
     </div>
   );
 };
 
-export default index;
+export default Index;
