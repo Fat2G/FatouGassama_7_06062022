e9a93321d79604dc33ba873fa3e0106dac4b8350
Fat2G <fatougassa@gmail.com>
2022-07-05 19:10:06 +0200

    ajout routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Profil from "./pages/Profil";
+
 
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/inscription" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profil" element={<Profil />} />
         {/* Si l'url rentré ne correspond à rien de déclaré, l'utilisateur est renvoyé a la page d'accueil */}
         <Route path="*" element={<Home />} />
       </Routes>
