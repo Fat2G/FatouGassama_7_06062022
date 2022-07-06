2ccf684179fea1b46c9141f54b5d4d1d2b65537f
Fat2G <fatougassa@gmail.com>
2022-07-07 01:26:48 +0200

    creation page dashboard

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,30 @@
+import { useEffect, React } from "react";
+import NavBar from "../components/NavBar";
+import CardPosts from "../components/CardPosts";
+import "../styles/pages/_dashboard.scss";
+import BackToTopButton from "../components/BackToTopButton";
+
+const Dashboard = () => {
+  // Utilisation du hook d'effet en enlevant la classe non voulue au body au profit d'une nouvelle classe.
+  useEffect(() => {
+    document.body.classList.remove("signup", "login", "profile");
+    document.body.classList.add("dashboard");
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <CardPosts />
+      <CardPosts />
+      <CardPosts />
+      <CardPosts />
+      <CardPosts />
+      <CardPosts />
+      <CardPosts />
+
+      <BackToTopButton />
+    </div>
+  );
+};
+
+export default Dashboard;
