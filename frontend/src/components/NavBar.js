9c954d6d20cf91dbdfd6ab72508d4df9b912ffcd
Fat2G <fatougassa@gmail.com>
2022-07-06 20:34:31 +0200

    création composant barre de navigation

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,94 @@
+import { React, useState } from "react";
+import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faRightFromBracket,
+  faCommentDots,
+  faCircleXmark,
+  faFolderPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import "../styles/components/_navbar.scss";
+import "../styles/components/_responsive.scss";
+
+const NavBar = () => {
+  //utilisation de la fonction modal afin de créer une fenêtre pop-up en utilisant le hook useState.
+  const [modal, setModal] = useState(false);
+
+  // puisque setModal est false il devient true et inversement
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  //On empêche de scroller lorsque le popup apparait avec un overflow-y: hidden en css
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
+  return (
+    <div>
+      <header>
+        <nav>
+          <NavLink className="ctn-logo" to="/dashboard">
+            <img src={logo} className="logo" alt="logo groupomania" />
+          </NavLink>
+          <div className="icons">
+            <FontAwesomeIcon
+              className="icon-navbar"
+              icon={faCommentDots}
+              onClick={toggleModal}
+            />
+            <NavLink className="icon-spe" to="/profil">
+              <FontAwesomeIcon className="icon-navbar" icon={faUser} />
+            </NavLink>
+            <FontAwesomeIcon
+              className="icon-navbar"
+              icon={faRightFromBracket}
+            />
+          </div>
+        </nav>
+      </header>
+
+      {/* Implémentation du "short circuit condition" afin de montrer ou cacher les éléments lorsque la condition (modal) est remplie.
+      Peut etre considéré comme une version minifié d'un opérateur ternaire. */}
+      {modal && (
+        <div className="modal">
+          <div className="overlay"></div>
+          <div className="modal-content">
+            <div className="modal-icons">
+              {/* ajout de fichier image */}
+              <FontAwesomeIcon
+                className="addImg-modal"
+                icon={faFolderPlus}
+              />
+              {/* fermeture du modal */}
+              <FontAwesomeIcon
+                className="close-modal"
+                onClick={toggleModal}
+                icon={faCircleXmark}
+              />
+            </div>
+
+            {/* message à publier */}
+            <textarea
+              id="post"
+              name="post"
+              rows="5"
+              cols="33"
+              placeholder="Que voulez-vous partager ?"
+              required
+            ></textarea>
+            <button type="button" className="btn">
+              Publier
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
