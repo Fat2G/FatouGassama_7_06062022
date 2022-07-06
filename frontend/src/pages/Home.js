51f6e4e5708afca13960f323ca41b341b38187a9
Fat2G <fatougassa@gmail.com>
2022-07-06 20:34:57 +0200

    ajout des classes a retirer du body

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,7 +5,7 @@ import HeaderLogo from "../components/HeaderLogo";
 const Index = () => {
   // Utilisation du hook d'effet en enlevant la classe non voulue au body au profit d'une nouvelle classe.
   useEffect(() => {
-    document.body.classList.remove("signup");
+    document.body.classList.remove("signup", "dashboard", "profile");
     document.body.classList.add("login");    
   }, []);
 
