4cb6b680c9b14524bf525650b1fe749e403efa30
Fat2G <fatougassa@gmail.com>
2022-07-13 14:26:34 +0200

    création fichier server.js

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const app = require("./app")
+
+/* configuration du port */
+// fonction qui renvoit un port valide
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+// gestion d'erreur
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+server.on("error", errorHandler);
+
+// écouteurs d'évènements consignant le port sur lequel le serveur s'éxecute
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+// écoute du port
+server.listen(port);
