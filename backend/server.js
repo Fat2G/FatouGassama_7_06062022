681efd20d699bb25edc644b0d36a1716d0896f2f
Fat2G <fatougassa@gmail.com>
2022-07-13 14:28:28 +0200

    correction syntaxe

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const http = require("http");
-const app = require("./app")
+const app = require("./app");
 
 /* configuration du port */
 // fonction qui renvoit un port valide
-const normalizePort = val => {
+const normalizePort = (val) => {
   const port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -19,19 +19,20 @@ const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 // gestion d'erreur
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
     throw error;
   }
   const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
       process.exit(1);
       break;
     default:
@@ -43,10 +44,10 @@ const server = http.createServer(app);
 server.on("error", errorHandler);
 
 // écouteurs d'évènements consignant le port sur lequel le serveur s'éxecute
-server.on('listening', () => {
+server.on("listening", () => {
   const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
 
 // écoute du port
