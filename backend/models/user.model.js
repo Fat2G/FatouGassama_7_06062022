b249e8b8ac4601dc8823c0a76e69410374191fba
Fat2G <fatougassa@gmail.com>
2022-07-21 18:49:52 +0200

    création fichier user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    admin: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+//utilisation de mongoose-unique-validator au schéma avant d'en faire un modèle
+userSchema.plugin(uniqueValidator);
+
+//export du schéma sous forme de modèle mongoose
+module.exports = mongoose.model("User", userSchema);
