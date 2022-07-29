5785d71d7bce11cff3c472fd9cc0992c2b67d2cd
Fat2G <fatougassa@gmail.com>
2022-07-29 11:58:01 +0200

    création middleware multer

---
+++ backend/middlewares/multer-config.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+
+//types de fichiers acceptés
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+
+// configuration multer
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    // 'null' = pas d'erreurs
+    // 'images' = nom du dossier créé pour stocker les images
+    callback(null, 'images')
+  },
+  // paramétrage du nouveau nom des fichiers images
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  }
+});
+
+module.exports = multer({storage}).single('image');
