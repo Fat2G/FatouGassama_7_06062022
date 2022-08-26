57a8afc64ce0e4539590d98fe7932d5e56a3d486
Fat2G <fatougassa@gmail.com>
2022-08-26 02:48:49 +0200

    fichier renommé

--- frontend/src/components/AppContext.js
+++
@@ -1,4 +0,0 @@
-// Import de createContext permettant de stocker des variables pouvant être appelés par n'importe quel component
-import {createContext} from 'react';
-
-export const UserIdContext = createContext();
