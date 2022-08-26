6a869479b061cd02079cb6ef56bbb0f5cc078007
Fat2G <fatougassa@gmail.com>
2022-08-26 02:47:34 +0200

    ajout reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,7 +2,9 @@
 import { combineReducers } from "redux";
 //import du state
 import userReducer from "./user.reducer";
+import usersReducer from "./users.reducer";
 
 export default combineReducers({
   userReducer,
+  usersReducer,
 });
