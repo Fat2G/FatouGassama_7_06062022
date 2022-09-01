7fd4ef4f718308de739cb92d9f9c0f4ba239d9de
Fat2G <fatougassa@gmail.com>
2022-09-02 01:25:59 +0200

    ajout upload picture

--- frontend/src/reducers/user.reducer.js
+++ frontend/src/reducers/user.reducer.js
@@ -1,4 +1,4 @@
-import {GET_USER} from '../actions/user.actions';
+import { GET_USER, UPLOAD_PICTURE } from "../actions/user.actions";
 // Etat de base des reducers (vide)
 const initialState = {};
 
@@ -7,6 +7,14 @@ export default function userReducer(state = initialState, action) {
     //dans le cas de GET_USER on incrémente l'initialState des données de payload et ces données seront accessibles par tous les components
     case GET_USER:
       return action.payload;
+
+    case UPLOAD_PICTURE:
+      return {
+        // on récupère les données de l'utilisateur en utilisant l'opérateur spread sans l'écraser
+        ...state,
+        // et on change l'image de profil
+        picture: action.payload,
+      };
 
     default:
       return state;
