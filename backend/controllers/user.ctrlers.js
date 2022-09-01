363095d9a9aa4fb3ee8bba9738eba4607f59bd7e
Fat2G <fatougassa@gmail.com>
2022-09-02 01:18:38 +0200

    uploadImg dans nouveau fichier + test delete

--- backend/controllers/user.ctrlers.js
+++ backend/controllers/user.ctrlers.js
@@ -2,6 +2,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const ObjectID = require("mongoose").Types.ObjectId;
+const fs = require("fs");
 
 // Variables d'environnement
 const secretToken = process.env.SECRET_TOKEN;
@@ -43,24 +44,65 @@ exports.userId = (req, res) => {
   }).select("-password");
 };
 
-
 // Suppression du compte utilisateur
 exports.deleteUser = (req, res) => {
   if (!ObjectID.isValid(req.params.id))
     return res.status(400).send("ID inconnu : " + req.params.id);
+  
+  User.findOne({ _id: req.params.id })
+    .then((user) => {
+      // suppression de l'image
+      const filename = user.picture.split("./uploads/profil/")[1];;
+      if (filename !== "defaultImg.jpg") {
+        fs.unlink(`./uploads/profil/${filename}`, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    })
+    .catch((err) => res.status(500).json({ err }));
 
-  User.remove({ _id: req.params.id })
+  User.deleteOne({ _id: req.params.id })
     .then(() => {
       res.cookie("jwt", "", { maxAge: 1 });
       res.redirect("/");
       res.status(200).json({ message: "Utilisateur supprimé ! " });
-      console.log('ok delete')
     })
     .catch((err) => {
       res.status(500).json({ message: err });
-      console.log('no delete')
     });
 };
+
+/* exports.deleteUser = (req, res) => {
+  User.findOne({ _id: req.params.id })
+    .then((user) => {
+      // suppression de l'image
+      const filename = user.picture.split("/images/users/")[1];
+      if (filename !== "defaultImg.jpg") {
+        fs.unlink(`images/users/${filename}`, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    })
+    .catch((err) => res.status(500).json({ err }));
+
+  // vérification des autorisations
+  if (req.params.id === req.token.userId) {
+    // suppressions du compte et du cookie
+    User.deleteOne({ _id: req.params.id })
+      .then(() => {
+        res.clearCookie("jwt");
+        res.redirect("/");
+      })
+      .catch((error) => res.status(400).json({ error }));
+  } else {
+    res.status(401).json({ error: "Non autorisé !" });
+  }
+};
+ */
 
 // Controle du token utilisateur du token
 exports.checkToken = (req, res, next) => {
@@ -80,4 +122,38 @@ exports.checkToken = (req, res, next) => {
     res.locals.user = null;
     next();
   }
+};
+
+// upload de l'image de profil
+exports.uploadImg = (req, res, next) => {
+  User.findOne({ _id: req.params.id })
+    .then((user) => {
+      // suppression de l'image par défaut
+      const filename = user.picture.split("./uploads/profil/")[1];
+      if (req.file && filename !== "defaultImg.jpg") {
+        fs.unlink(`./uploads/profil/${filename}`, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+
+      // update de l'image
+      if (req.params.id === req.token.userId) {
+        User.updateOne(
+          { _id: req.params.id },
+          {
+            picture: `${req.protocol}://${req.get("host")}/uploads/profil/${
+              req.file.filename
+            }`,
+            _id: req.params.id,
+          }
+        )
+          .then(() => res.status(200).json({ message: "Image mise à jour !" }))
+          .catch((error) => res.status(400).json({ error }));
+      } else {
+        res.status(401).json({ error: "Non autorisé" });
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
