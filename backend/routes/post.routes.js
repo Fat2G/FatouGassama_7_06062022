3f495d902e007383a4a10ca69d891bb70d15447a
Fat2G <fatougassa@gmail.com>
2022-09-02 01:34:20 +0200

    modif route pour update et delete

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -6,7 +6,7 @@ const postCtrl = require('../controllers/post.ctrlers');
 //CRUD
 router.get('/', postCtrl.readPost);
 router.post('/', postCtrl.createPost);
-router.put('/', postCtrl.updatePost);
-router.delete('/', postCtrl.deletePost);
+router.put('/:id', postCtrl.updatePost);
+router.delete('/:id', postCtrl.deletePost);
 
 module.exports = router;
