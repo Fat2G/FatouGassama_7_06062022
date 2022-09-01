0eb7f3e55920b82c1f26b6036f46ab6da2a6451d
Fat2G <fatougassa@gmail.com>
2022-09-02 01:20:03 +0200

    ajout reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.scss";
+import App from "./App";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { getUsers } from "./actions/users.actions";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// outil de développement
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// création du store
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.dispatch(getUsers());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
