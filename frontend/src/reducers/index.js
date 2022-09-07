d441bdcd104e42043c159119c9eae89bd69497ae
Fat2G <fatougassa@gmail.com>
2022-09-07 02:49:23 +0200

    ajout error reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -3,8 +3,10 @@ import { combineReducers } from "redux";
 //import du state
 import userReducer from "./user.reducer";
 import usersReducer from "./users.reducer";
+import errorReducer from "./error.reducer";
 
 export default combineReducers({
   userReducer,
   usersReducer,
+  errorReducer
 });
