657ad26d45176f4278a9cfc268c64a7ac37ca514
Fat2G <fatougassa@gmail.com>
2022-09-07 02:51:04 +0200

    retrait dislikes et usersDisliked

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const postSchema = new mongoose.Schema(
   {
-    PosterId: { type: String, required: true },
+    posterId: { type: String, required: true },
     message: { type: String, trim: true, maxLength: 1000 },
     picture: { type: String },
     likes: { type: Number, default: 0, required: true },
-    dislikes: { type: Number, default: 0, required: false },
     usersLiked: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    usersDisliked: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
