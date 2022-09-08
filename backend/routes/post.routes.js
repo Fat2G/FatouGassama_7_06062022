c80dbc771c3bc2ccef33fcead4efd743073049ca
Fat2G <fatougassa@gmail.com>
2022-09-08 02:48:45 +0200

    ajout des routes like et unlike

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 //import du controller post
-const postCtrl = require('../controllers/post.ctrlers');
+const postCtrl = require("../controllers/post.ctrlers");
 
 //CRUD
-router.get('/', postCtrl.readPost);
-router.post('/', postCtrl.createPost);
-router.put('/:id', postCtrl.updatePost);
-router.delete('/:id', postCtrl.deletePost);
+router.get("/", postCtrl.readPost);
+router.post("/", postCtrl.createPost);
+router.put("/:id", postCtrl.updatePost);
+router.delete("/:id", postCtrl.deletePost);
+
+//likes
+router.patch("/like-post/:id", postCtrl.likePost);
+router.patch("/unlike-post/:id", postCtrl.unlikePost);
 
 module.exports = router;
