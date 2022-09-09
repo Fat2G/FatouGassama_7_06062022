b44721b574d1ca4f3dbd7ba2fd569727f590f8cf
Fat2G <fatougassa@gmail.com>
2022-09-09 19:32:52 +0200

    ajout commentaire

--- frontend/src/components/UploadImg.js
+++ frontend/src/components/UploadImg.js
@@ -1,29 +1,26 @@
 import { useState, React } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import axios from "axios";
 import { uploadPic } from "../actions/user.actions";
 
 const UploadImg = () => {
   const [file, setFile] = useState();
   // récupération des données de l'utilisateur stockées dans le reducer
   const userData = useSelector((state) => state.userReducer);
-  // action qui envoie l'image vers le backend
+  // action qui dispatch l'image vers le store
   const dispatch = useDispatch();
 
   const handlePic = (e) => {
     e.preventDefault();
     //objet permettant de rassembler l'image et les informations eventuelles
-    const formData = new FormData();
-    formData.append("name", userData.username);
-    formData.append("userId", userData._id);
-    formData.append("file", file);
+    const data = new FormData();
+    data.append("name", userData.username);
+    data.append("userId", userData._id);
+    data.append("file", file);
 
-    dispatch(uploadPic(formData, userData._id));
+    dispatch(uploadPic(data, userData._id));
   };
 
-  /**
-   * If the file input is not empty, enable the submit button
-   */
+  // Si l'input file est vide le bouton envoyer est désactivé
   const btnDisabled = () => {
     let picFile = document.querySelector("#file").file;
 
