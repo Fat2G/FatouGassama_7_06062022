2cbff543193f5f9478a66c5bf88de621e9ae03fc
Fat2G <fatougassa@gmail.com>
2022-09-14 21:38:01 +0200

    ajout postReducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -4,9 +4,11 @@ import { combineReducers } from "redux";
 import userReducer from "./user.reducer";
 import usersReducer from "./users.reducer";
 import errorReducer from "./error.reducer";
+import postReducer from "./post.reducer";
 
 export default combineReducers({
   userReducer,
   usersReducer,
-  errorReducer
+  errorReducer,
+  postReducer,
 });
