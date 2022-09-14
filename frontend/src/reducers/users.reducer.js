bfadc31eb5ff5fc6a0315e1827b88950f2503bbc
Fat2G <fatougassa@gmail.com>
2022-09-14 21:38:27 +0200

    indentation

--- frontend/src/reducers/users.reducer.js
+++ frontend/src/reducers/users.reducer.js
@@ -2,7 +2,7 @@ import { GET_USERS } from "../actions/users.actions";
 // Etat de base des reducers (vide)
 const initialState = {};
 
-export default function userReducer(state = initialState, action) {
+export default function usersReducer(state = initialState, action) {
   switch (action.type) {
     //dans le cas de GET_USERS on incrémente l'initialState des données de payload et ces données seront accessibles par tous les components
     case GET_USERS:
